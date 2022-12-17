cb4928dd8f7e3b41fb8ce3cb3b2ad4edc34c9821
MarcioBADias <98918078+MarcioBADias@users.noreply.github.com>
2022-12-17 09:27:45 -0300

    Atualização de projeto
    teste de touch e refatoração de codigo

--- js/app.js
+++ js/app.js
@@ -11,29 +11,45 @@ const jumpMario = () => {
         mario.classList.remove('jump');
     }, 800)
 }
-console.log(pipe)
+
+const printScore = () => {
+    score += 10;
+    counter.textContent = `${score} pontos`
+}
+
+
 const endGame = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    console.log(marioPosition)
-    score += 10;
-    counter.textContent = `${score} pontos`
-    if(pipePosition <= 60 && marioPosition <= 70){
+    const jumpFail = pipePosition <= 60 && marioPosition <= 70;
+    const speed2 = score > 1200;
+    const speed3 = score > 3000;
+    const stopPipe = () => {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-
+    }
+    
+    const stopMario = () => {
         mario.style.bottom = `${marioPosition}px`;
         mario.src = './assents/img/mario-dead.gif';
+    }
+
+    printScore();
+
+    if(jumpFail){
+        stopPipe();
+        stopMario();
         clearInterval(endGame);      
     }
 
-    if(score > 1200){
+    if(speed2){
         pipe.style.animation = 'pipe-animate 1500ms infinite linear'
     }
 
-    if(score > 3000){
+    if(speed3){
         pipe.style.animation = 'pipe-animate 1s infinite linear'
     }
 }, 50);
 
-document.addEventListener('keydown', jumpMario)
+document.addEventListener('keydown', jumpMario);
+document.addEventListener('touchstart', jumpMario);
